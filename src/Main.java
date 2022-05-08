c2d526b298fdb7d5dc374d815e3729e6056e8fd8
Mrwhitedeath <belovsergey@mail.ru>
2022-05-08 16:34:05 +0300

    create class numbers for operation with numbers

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,8 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите уравнение:");
-
-        String input = scanner.nextLine();
+    public static String calc (String input){
         // Нормализуем ввод, убираем все пробелы, чтобы работало если строку ввели с пробелами и без
         input = input.replace(" ", "");
 
@@ -22,10 +18,45 @@ public class Main {
             System.out.println("Введено некорректное уравнение.");
             System.exit(1);
         }
+        // теперь надо проверить , что числа корректны
+
+
+
+
 
         System.out.println(Arrays.asList(inputStr));
 
-
-
+        return "";
     }
+
+    public boolean isArabic (String digit){
+        int dig = Integer.parseInt(digit);
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите уравнение:");
+
+        String input = scanner.nextLine();
+
+        String result = calc(input);
+
+        System.out.println(result);
+    }
+}
+//Создаем класс , чтобы все операции с числами производить с помощью методов данного класса
+class numbers{
+    boolean isArabic(String num){             // проверяем, что число арабское и  0< num <=10
+        boolean result;
+        try {
+            int digit = Integer.parseInt(num);
+            result = (digit > 0) && (digit <= 10);
+        } catch (NumberFormatException e) { // если распарсить не получилось, значит точно не арабское число
+            result = false;
+        }
+        return result;
+    }
+
 }
