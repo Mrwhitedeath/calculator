3e0facddab9675636410a2a24fed1e564306552d
Mrwhitedeath <belovsergey@mail.ru>
2022-05-08 18:17:56 +0300

    Create enum RomanDigit

---
+++ src/RomanDigit.java
@@ -0,0 +1,9 @@
+public enum RomanDigit {
+    I(1), II(2), III(3), IV(4), V(5), VI(6), VII(7), VIII(8), IX(9), X(10);
+
+    private int arabianDigit;
+
+    RomanDigit(int arabianDigit) {
+        this.arabianDigit = arabianDigit;
+    }
+}
