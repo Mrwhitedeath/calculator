0f8601e9da4756f3e36a4b0879787842dac0b4aa
Mrwhitedeath <belovsergey@mail.ru>
2022-05-09 07:42:44 +0300

    change to static methods

--- src/Main.java
+++ src/Main.java
@@ -103,7 +103,7 @@ class Numbers{
 
     static int romanToArabic(String string){
         int res = 0;
-        HashMap<Character, Integer> romanian = new HashMap<>();
+        final HashMap<Character, Integer> romanian = new HashMap<>();
         romanian.put('I', 1);
         romanian.put('V', 5);
         romanian.put('X', 10);
@@ -124,8 +124,8 @@ class Numbers{
     }
 
     static public String arabicToRoman(int num) {
-        int[] value = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        String[] roman = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        final int[] value = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        final String[] roman = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         StringBuilder res = new StringBuilder();
 
         for (int i=0; i < value.length; i++){
